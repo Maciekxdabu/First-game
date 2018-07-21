f1877a2be21d0b13e868253478be03a173f4d81d
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-07-21 19:36:40 +0200

    wprowadzanie wyświetlania mapy

--- segment.cpp
+++ segment.cpp
@@ -1,8 +1,12 @@
 #include "segment.h"
 
-segment::segment()
+segment::segment(std::string adres_obrazu, int xx, int yy)
 {
-    ;
+    tekstura.loadFromFile(adres_obrazu);
+    obraz.setTexture(tekstura);
+    obraz.setPosition(xx*width, yy*height);
+    x = xx;
+    y = yy;
 }
 
 segment::~segment()
@@ -20,35 +24,12 @@ int segment::getObiekt(int n)
     return zawartosc[n];
 }
 
-int segment::getIloscMob()
-{
-    return moby.getLenght();
-}
-
-int segment::getMob(int n)
-{
-    return moby[n];
-}
-
-int segment::getIloscPoz()
-{
-    return pozostale.getLenght();
-}
-
-int segment::getPoz(int n)
-{
-    return pozostale[n];
-}
-
 void segment::dodaj(ob_fiz* ob)
 {
     zawartosc.push_back(ob);
-    switch (ob->getTag())
-    {
-    case 'm':
-        moby.push_back(ob);
-        break;
-    default:
-        pozostale.push_back(ob);
-    }
+}
+
+sf::Sprite segment::getObraz()
+{
+    return obraz;
 }
