b33f3179c581d4fedc5ef3d0edc4e9114a81af28
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-07-25 00:14:08 +0200

    system kolizji już prawie gotowy

--- Postac.cpp
+++ Postac.cpp
@@ -3,7 +3,7 @@
 Postac::Postac(float px, float py, float kx, float ky, float ks, float kw, std::string adres)
 :ob_fiz(px, py, kx, ky, ks, kw, adres)
 {
-    ;
+    tag = postac;
 }
 
 Postac::~Postac()
@@ -11,37 +11,37 @@ Postac::~Postac()
     ;
 }
 
-void Postac::wyrownaj(Granica ob)
+void Postac::wyrownaj(ob_fiz *ob)
 {
-    if (czy_kol(this, &ob))
+    if (czy_kol(this, ob))
     {
-        switch (ob.getKier())
+        switch (ob->getKier())
         {
         case gora:
-            if (ob.getSpeed().y <= 0)
+            if (ob->getSpeed().y <= 0)
             {
-                obraz.setPosition(obraz.getPosition().x, ob.getPos().y);
+                obraz.setPosition(obraz.getPosition().x, ob->getPos().y);
                 predkosc.y = 0;
             }
             break;
         case dol:
-            if (ob.getSpeed().y >= 0)
+            if (ob->getSpeed().y >= 0)
             {
-                obraz.setPosition(obraz.getPosition().x, ob.getPos().y - wysokosc);
+                obraz.setPosition(obraz.getPosition().x, ob->getPos().y - wysokosc);
                 predkosc.y = 0;
             }
             break;
         case lewo:
-            if (ob.getSpeed().x <= 0)
+            if (ob->getSpeed().x <= 0)
             {
-                obraz.setPosition(ob.getPos().x, obraz.getPosition().y);
+                obraz.setPosition(ob->getPos().x, obraz.getPosition().y);
                 predkosc.x = 0;
             }
             break;
         case prawo:
-            if (ob.getSpeed().x >= 0)
+            if (ob->getSpeed().x >= 0)
             {
-                obraz.setPosition(ob.getPos().x - szerokosc, obraz.getPosition().y);
+                obraz.setPosition(ob->getPos().x - szerokosc, obraz.getPosition().y);
                 predkosc.x = 0;
             }
             break;
