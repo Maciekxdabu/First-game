834a5c987cf6ba88a0101e57a3a8ff715f441fd2
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-07-25 00:09:29 +0200

    system kolizji prawie gotowy

--- klasy.h
+++ klasy.h
@@ -3,13 +3,11 @@
 
 enum Tag /// zmienne typu Tag odpowiadają przede wszystkim za odróżnianie poszczególnych klas
 {
-    mob,
     statyczny,
     interaktywny,
-    NONE,
-    gracz,
-    shelf,
-    granica
+    postac,
+    granica,
+    NONE
 };
 enum Kier /// zmienna typu Kier odpowiedzialna jest za zapamietywanie kierunkow
 {
@@ -32,6 +30,7 @@ protected:
     Tag tag; /// przechowuje wartosc tag odrozniajaca obiekty (jak 'm' dla moba, czy 'f' dla obiektu fizycznego)
     sf::Sprite obraz; /// obraz obiektu fizycznego
     sf::Texture tekstura; /// i jego tekstura
+    Kier kierunek;
 
 public:
     ob_fiz(float, float, float, float, float, float, std::string);
@@ -40,8 +39,10 @@ public:
     Tag getTag(); /// zwraca tag
     sf::Vector2f getSpeed();
     sf::Vector2f getPos();
-    void addVector(int=0, int=0);
+    sf::Sprite getObraz();
+    void addVector(float=0, float=0);
     void ruch();
+    Kier getKier();
 
     friend bool czy_kol(ob_fiz*, ob_fiz*);
 };
@@ -76,6 +77,11 @@ protected:
     sf::Sprite obraz;
     sf::Texture tekstura;
     int x, y; /// wspolrzedne segmentu w mapie
+    int ilosc_postaci;
+    int ilosc_statycznych;
+    int ilosc_interaktywnych;
+    int ilosc_pozostalych;
+    int ilosc_granic;
 
 public:
     segment(std::string, int, int);
@@ -83,6 +89,11 @@ public:
     int getIlosc(); /// zwraca ilosc elementow w segmencie mapy
     ob_fiz* getObiekt(int); /// zwraca obiekt o podanym indeksie
     sf::Sprite getObraz(); /// zwraca obiekt Sprite do wyswietlenia w oknie
+    int getPos();
+    int getStat();
+    int getInt();
+    int getPoz();
+    int getGran();
 
     void dodaj(ob_fiz*); /// dodaje nowy element z segmentu
     void usun(); /// usuwa element z segmentu ---TO DO---
@@ -93,12 +104,12 @@ public:
 class Granica :public ob_fiz
 {
 protected:
-    Kier kierunek;
+    //Kier kierunek;
 
 public:
     Granica(float, float, float, float, float, float, std::string, Kier);
     ~Granica();
-    Kier getKier();
+    //Kier getKier();
 };
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------ klasa Postac
@@ -112,7 +123,7 @@ protected:
 public:
     Postac(float, float, float, float, float, float, std::string);
     ~Postac();
-    virtual void wyrownaj(Granica);
+    virtual void wyrownaj(ob_fiz*);
 };
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------ klasa Player
