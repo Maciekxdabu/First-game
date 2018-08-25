6fc59e69a743bc4ab120b1afe650f45033b0cae8
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-08-25 18:55:44 +0200

    wprowadzam system interakcji

--- Player.cpp
+++ Player.cpp
@@ -5,6 +5,10 @@ Player::Player(float px, float py, float kx, float ky, float ks, float kw, std::
 {
     maxx = 2;
     maxy = 2;
+    
+    numer_segmentu = s;
+
+    interakcja = false;
 }
 
 Player::~Player()
@@ -25,4 +29,55 @@ void Player::LVup()
     EXP = EXP - MAXEXP;
     //MAXEXP =
     LV++;
+}
+
+void Player::ruch()
+{
+    ob_fiz::ruch();
+
+    widok_G.setCenter(obraz.getPosition().x + 50, obraz.getPosition().y + 50);
+    okno.setView(widok_G);
+}
+
+void Player::czy_interakcja(Interaktywne* ob)
+{
+    if ( ob_fiz::czy_kol(this, ob) )
+    {
+        mozliwe = ob->getID();
+        interakcja = true;
+    }
+}
+
+void Player::teleportacja(float xx, float yy)
+{
+    float zmx = xx - (obraz.getPosition().x + szerokosc/2);
+    float zmy = yy - (obraz.getPosition().y + wysokosc/2);
+
+    obraz.move(zmx, zmy);
+    x += zmx;
+    y += zmy;
+}
+
+void Player::reset()
+{
+    interakcja = false;
+    mozliwe = -1;
+}
+
+void Player::zmien_segment(int s)
+{
+    numer_segmentu = s;
+}
+
+int Player::getSegment()
+{
+    return numer_segmentu;
+}
+
+int Player::getInt()
+{
+    if (interakcja == true)
+        return mozliwe;
+    else
+        return -1;
 }
