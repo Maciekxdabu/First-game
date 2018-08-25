30fe75e687a9c2a38674925337b208cb1a09b7fc
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-08-25 18:52:46 +0200

    wprowadzam system interakcji

--- Interaktywne.cpp
+++ Interaktywne.cpp
@@ -10,3 +10,21 @@ Interaktywne::~Interaktywne()
 {
     ;
 }
+
+// ----------------------------------------------------------------------- Drzwi
+
+Drzwi::Drzwi(float px, float py, float kx, float ky, float ks, float kw, std::string adres, float xx, float yy)
+:Interaktywne(px, py, kx, ky, ks, kw, adres)
+{
+    cel = sf::Vector2f(xx, yy);
+}
+
+Drzwi::~Drzwi()
+{
+    ;
+}
+
+void Drzwi::interakcja()
+{
+    bohater.teleportacja(cel.x, cel.y);
+}
